9e26b5a2e7d98f3333a2d73847dd6f34a784078f
zoro0611innodisk <119554188+zoro0611innodisk@users.noreply.github.com>
2023-09-25 08:42:17 +0800

    加入專案檔案。

---
+++ netcore_supabase/Model/Menu.cs
@@ -0,0 +1,24 @@
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netcore_supabase.Model
+{
+    public class Menu : BaseModel
+    {
+        [PrimaryKey("id")]
+        public int Id { get; set; }
+
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime Created_At { get; set; }
+
+    }
+}
---
+++ netcore_supabase/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using netcore_supabase.Model;
+
+var builder = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddUserSecrets<Program>(); // 添加UserSecrets
+IConfigurationRoot configuration = builder.Build();
+string dbConnectionString = configuration.GetConnectionString("DatabaseConnection");
+
+string usersecret_superbaseUrl = configuration["Superbase:Url"];
+string usersecret_superbaseKey = configuration["Superbase:Key"];
+
+var options = new Supabase.SupabaseOptions
+{
+    AutoConnectRealtime = true
+};
+
+var supabase = new Supabase.Client(usersecret_superbaseUrl, usersecret_superbaseKey, options);
+await supabase.InitializeAsync();
+
+var result = await supabase.From<Menu>().Get();
+var menus = result.Models.OrderBy(x=>x.Id);
+
+foreach (var menu in menus)
+{
+    Console.WriteLine($"Id: {menu.Id}, Stock: {menu.Stock}, Price: {menu.Price}, Title: {menu.Title}, image: {menu.Image}, categoryId: {menu.CategoryId}");
+}
+
+
+
+Console.ReadLine();
